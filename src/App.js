335edaf93e010fef55708f519542e124c199dd97
pbueoka <ueoka@primebrains.co.jp>
2022-08-27 11:14:59 +0900

    Firebaseとの疎通確認（送信後のレンダリングでエラー。。。）

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { db } from './firebase'
 import './App.css';
 
 function App() {
+
+  const room = "chat_room";
+
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [output, setOutput] = useState("");
+
+  //name属性でstate名を判断する条件分岐　
+  const handleChange = (e) => {
+      if(e.target.name === 'name') {
+        setName (e.target.value);
+      } else if (e.target.name === 'message') {
+        setMessage (e.target.value);
+      }
+  }
+
+  //送信処理
+  function send() {
+    var now = new Date();
+    db.ref(room).push({
+        name: name,
+        message: message,
+        date: now.getFullYear() + '年' + now.getMonth()+1 + '月' + now.getDate() + '日' + now.getHours() + '時' + now.getMinutes() + '分'
+    });
+    setName("");
+    setMessage("");
+  };
+
+  //受信処理
+  db.ref(room).on("child_added", function(data) {
+      const v = data.val();
+      const k = data.key;
+      let str = "";
+      str += '<div class="name">名前：'+v.name+'</div>';
+      str += '<div class="text">日時：'+v.date+'</div>';
+      str += '<div class="text">メッセージ：'+v.message+'</div><hr>';
+      // setOutput(output + str);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +57,20 @@ function App() {
         >
           Learn Reacts
         </a>
-      </header>
+      </header> */}
+      <h1>リアルタイムチャット</h1>
+      <div>
+          <div>
+              Name<br/><input type="text" name="name" value={name} onChange={handleChange} />
+          </div>
+          <br/>
+          <div>
+              Message<br/><textarea row="10" name="message" value={message} onChange={handleChange}></textarea><br/><br/>
+              <button onClick={send}>send</button>
+          </div>
+          <hr/>
+          <div value={output} ></div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,38 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import firebase from 'firebase/app'
-
-var database = firebase.database();
-let room = "chat_room";
-const send = document.getElementById("send");
-const name = document.getElementById("name");
-const message = document.getElementById("message");
-const output = document.getElementById("output");
-
-//送信処理
-send.addEventListener('click', function() {
-    var now = new Date();
-    database.ref(room).push({
-        name: name.value,
-        message: message.value,
-        date: now.getFullYear() + '年' + now.getMonth()+1 + '月' + now.getDate() + '日' + now.getHours() + '時' + now.getMinutes() + '分'
-    });
-    message.value="";
-    name.value="";
-});
-
-//受信処理
-database.ref(room).on("child_added", function(data) {
-    const v = data.val();
-    const k = data.key;
-    let str = "";
-    str += '<div class="name">名前：'+v.name+'</div>';
-    str += '<div class="text">日時：'+v.date+'</div>';
-    str += '<div class="text">メッセージ：'+v.message+'</div><hr>';
-    output.innerHTML += str;
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -41,8 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
